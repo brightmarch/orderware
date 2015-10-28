4c8318ffc08e521ed714dd1dd2ccdee2f8a1b0eb
Vic Cherubini <vic@leftnode.org>
2015-10-27 22:35:49 -0500

    Got the basic entities and library and doctrine entities set up.

---
+++ src/Orderware/AppBundle/Entity/Division.php
@@ -0,0 +1,423 @@
+<?php
+
+namespace Orderware\AppBundle\Entity;
+
+/**
+ * Division
+ */
+class Division
+{
+
+    /**
+     * @var string
+     */
+    private $division;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     */
+    private $createdBy;
+
+    /**
+     * @var string
+     */
+    private $updatedBy;
+
+    /**
+     * @var integer
+     */
+    private $statusId;
+
+    /**
+     * @var string
+     */
+    private $displayName;
+
+    /**
+     * @var string
+     */
+    private $currency;
+
+    /**
+     * @var string
+     */
+    private $timeZone;
+
+    /**
+     * @var string
+     */
+    private $primaryEmail;
+
+    /**
+     * @var string
+     */
+    private $notificationEmail;
+
+    /**
+     * @var string
+     */
+    private $merchDescription;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $users;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set division
+     *
+     * @param string $division
+     *
+     * @return Division
+     */
+    public function setDivision($division)
+    {
+        $this->division = $division;
+
+        return $this;
+    }
+
+    /**
+     * Get division
+     *
+     * @return string
+     */
+    public function getDivision()
+    {
+        return $this->division;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Division
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Division
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     *
+     * @return Division
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param string $updatedBy
+     *
+     * @return Division
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return string
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Set statusId
+     *
+     * @param integer $statusId
+     *
+     * @return Division
+     */
+    public function setStatusId($statusId)
+    {
+        $this->statusId = $statusId;
+
+        return $this;
+    }
+
+    /**
+     * Get statusId
+     *
+     * @return integer
+     */
+    public function getStatusId()
+    {
+        return $this->statusId;
+    }
+
+    /**
+     * Set displayName
+     *
+     * @param string $displayName
+     *
+     * @return Division
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+
+        return $this;
+    }
+
+    /**
+     * Get displayName
+     *
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * Set currency
+     *
+     * @param string $currency
+     *
+     * @return Division
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = strtoupper($currency);
+
+        return $this;
+    }
+
+    /**
+     * Get currency
+     *
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Set timeZone
+     *
+     * @param string $timeZone
+     *
+     * @return Division
+     */
+    public function setTimeZone($timeZone)
+    {
+        $this->timeZone = $timeZone;
+
+        return $this;
+    }
+
+    /**
+     * Get timeZone
+     *
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * Set primaryEmail
+     *
+     * @param string $primaryEmail
+     *
+     * @return Division
+     */
+    public function setPrimaryEmail($primaryEmail)
+    {
+        $this->primaryEmail = strtolower($primaryEmail);
+
+        return $this;
+    }
+
+    /**
+     * Get primaryEmail
+     *
+     * @return string
+     */
+    public function getPrimaryEmail()
+    {
+        return $this->primaryEmail;
+    }
+
+    /**
+     * Set notificationEmail
+     *
+     * @param string $notificationEmail
+     *
+     * @return Division
+     */
+    public function setNotificationEmail($notificationEmail)
+    {
+        $this->notificationEmail = strtolower($notificationEmail);
+
+        return $this;
+    }
+
+    /**
+     * Get notificationEmail
+     *
+     * @return string
+     */
+    public function getNotificationEmail()
+    {
+        return $this->notificationEmail;
+    }
+
+    /**
+     * Set merchDescription
+     *
+     * @param string $merchDescription
+     *
+     * @return Division
+     */
+    public function setMerchDescription($merchDescription)
+    {
+        $this->merchDescription = $merchDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get merchDescription
+     *
+     * @return string
+     */
+    public function getMerchDescription()
+    {
+        return $this->merchDescription;
+    }
+
+    /**
+     * Add user
+     *
+     * @param \Orderware\AppBundle\Entity\User $user
+     *
+     * @return Division
+     */
+    public function addUser(\Orderware\AppBundle\Entity\User $user)
+    {
+        $this->users[] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param \Orderware\AppBundle\Entity\User $user
+     */
+    public function removeUser(\Orderware\AppBundle\Entity\User $user)
+    {
+        $this->users->removeElement($user);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onCreate()
+    {
+        $this->setCreatedAt(date_create())
+            ->setUpdatedAt(date_create());
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onUpdate()
+    {
+        $this->setUpdatedAt(date_create());
+    }
+
+}
---
+++ src/Orderware/AppBundle/Entity/User.php
@@ -0,0 +1,501 @@
+<?php
+
+namespace Orderware\AppBundle\Entity;
+
+use Orderware\AppBundle\Library\Constants;
+use Orderware\AppBundle\Library\Status;
+
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Encoder\EncoderAwareInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+use \Serializable;
+
+/**
+ * User
+ */
+class User implements
+    AdvancedUserInterface,
+    EncoderAwareInterface,
+    EquatableInterface,
+    Serializable
+{
+
+    /**
+     * @var integer
+     */
+    private $loginId;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     */
+    private $createdBy;
+
+    /**
+     * @var string
+     */
+    private $updatedBy;
+
+    /**
+     * @var integer
+     */
+    private $statusId;
+
+    /**
+     * @var string
+     */
+    private $fullName;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $passwordHash;
+
+    /**
+     * @var string
+     */
+    private $timeZone;
+
+    /**
+     * @var string
+     */
+    private $role;
+
+    /**
+     * @var \Orderware\AppBundle\Entity\Division
+     */
+    private $division;
+
+    /**
+     * To string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getUsername();
+    }
+
+    /**
+     * Get loginId
+     *
+     * @return integer
+     */
+    public function getLoginId()
+    {
+        return $this->loginId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     *
+     * @return User
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param string $updatedBy
+     *
+     * @return User
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return string
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Set statusId
+     *
+     * @param integer $statusId
+     *
+     * @return User
+     */
+    public function setStatusId($statusId)
+    {
+        $this->statusId = $statusId;
+
+        return $this;
+    }
+
+    /**
+     * Get statusId
+     *
+     * @return integer
+     */
+    public function getStatusId()
+    {
+        return $this->statusId;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     *
+     * @return User
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set passwordHash
+     *
+     * @param string $passwordHash
+     *
+     * @return User
+     */
+    public function setPasswordHash($passwordHash)
+    {
+        $this->passwordHash = $passwordHash;
+
+        return $this;
+    }
+
+    /**
+     * Get passwordHash
+     *
+     * @return string
+     */
+    public function getPasswordHash()
+    {
+        return $this->passwordHash;
+    }
+
+    /**
+     * Set timeZone
+     *
+     * @param string $timeZone
+     *
+     * @return User
+     */
+    public function setTimeZone($timeZone)
+    {
+        $this->timeZone = $timeZone;
+
+        return $this;
+    }
+
+    /**
+     * Get timeZone
+     *
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return User
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
+    {
+        return $this->passwordHash;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setPassword($password)
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setSalt($salt)
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        $role = new Role($this->role);
+
+        return [$role];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEnabled()
+    {
+        return (Status::ENABLED === $this->statusId);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function serialize()
+    {
+        return serialize([$this->getLoginId(), $this->username]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function unserialize($serialized)
+    {
+        list($this->loginId, $this->username) = unserialize($serialized);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEqualTo(UserInterface $user)
+    {
+        return ($this->username === $user->getUsername());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getEncoderName()
+    {
+        return Constants::ENCODER_STATELESS;
+    }
+
+    /**
+     * Set division
+     *
+     * @param \Orderware\AppBundle\Entity\Division $division
+     *
+     * @return User
+     */
+    public function setDivision(\Orderware\AppBundle\Entity\Division $division = null)
+    {
+        $this->division = $division;
+
+        return $this;
+    }
+
+    /**
+     * Get division
+     *
+     * @return \Orderware\AppBundle\Entity\Division
+     */
+    public function getDivision()
+    {
+        return $this->division;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onCreate()
+    {
+        $this->setCreatedAt(date_create())
+            ->setUpdatedAt(date_create());
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onUpdate()
+    {
+        $this->setUpdatedAt(date_create());
+    }
+
+}
