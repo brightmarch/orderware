3c592a527240718008cb3458eddf708c0705ddb4
Vic Cherubini <vic@leftnode.org>
2015-10-28 14:50:24 -0500

    New fixture to create the User entities.

--- src/Orderware/AppBundle/Tests/TestCase.php
+++ src/Orderware/AppBundle/Tests/TestCase.php
@@ -8,7 +8,8 @@ abstract class TestCase extends \Liip\FunctionalTestBundle\Test\WebTestCase
     public function setUp()
     {
         $fixtures = $this->loadFixtureFiles([
-            '@OrderwareAppBundle/Resources/config/fixtures/Division.yml'
+            '@OrderwareAppBundle/Resources/config/fixtures/Division.yml',
+            '@OrderwareAppBundle/Resources/config/fixtures/User.yml'
         ]);
     }
 
