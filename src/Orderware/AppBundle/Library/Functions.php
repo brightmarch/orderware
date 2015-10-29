ef378c5485b3757a53be355adb1238ac081995b7
Vic Cherubini <vic@leftnode.org>
2015-10-28 21:47:13 -0500

    Decided to rename the Utils class to Functions. We'll see how long that lasts.

---
+++ src/Orderware/AppBundle/Library/Functions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Orderware\AppBundle\Library;
+
+class Functions
+{
+
+    /**
+     * Returns a value of an array by key.
+     *
+     * @param array $array
+     * @param mixed $key
+     */
+    public static function arrayValue(array $array, $key)
+    {
+        if (!is_scalar($key) || !array_key_exists($key, $array)) {
+            return null;
+        }
+
+        return $array[$key];
+    }
+
+    /**
+     * Returns a database representation of a boolean value.
+     *
+     * @param boolean $value
+     * @return string
+     */
+    public static function dbBool($value)
+    {
+        return (!(bool)$value ? 'f' : 't');
+    }
+
+    /**
+     * Creates an ISO timestamp in YYYY-MM-DD HH:MM:SS format.
+     *
+     * @return string
+     */
+    public static function dbDate()
+    {
+        return date('Y-m-d H:i:s');
+    }
+
+    /**
+     * Generates a non-secure random string.
+     *
+     * @param integer $length
+     * @return string
+     */
+    public static function randomString($length = 8)
+    {
+        $length = abs((int)$length);
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $poolLength = strlen($pool) - 1;
+
+        $random = '';
+
+        for ($i=0; $i<$length; $i++) {
+            $random .= $pool[mt_rand(0, $poolLength)];
+        }
+        
+        return $random;
+    }
+
+}
---
+++ src/Orderware/AppBundle/Tests/Library/FunctionsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Orderware\AppBundle\Tests\Library;
+
+use Orderware\AppBundle\Library\Functions;
+
+use \DateTime;
+
+class FunctionsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGettingArrayValueReturnsNullByDefault()
+    {
+        $this->assertNull(Functions::arrayValue([], 'key'));
+    }
+
+    public function testGettingArrayValueRequiresScalarKey()
+    {
+        $this->assertNull(Functions::arrayValue(['key' => 'a'], new \StdClass));
+    }
+
+    public function testGettingArrayValue()
+    {
+        $this->assertEquals('a', Functions::arrayValue(['key' => 'a'], 'key'));
+    }
+
+    public function testGettingDatabaseBooleanValue()
+    {
+        $this->assertEquals('f', Functions::dbBool(false));
+        $this->assertEquals('t', Functions::dbBool(true));
+    }
+
+    public function testGettingDatabaseDate()
+    {
+        $this->assertEquals(date('Y-m-d H:i:s'), Functions::dbDate());
+    }
+
+    /**
+     * @group FragileTests
+     */
+    public function testGeneratingRandomString()
+    {
+        $this->assertNotEquals(
+            Functions::randomString(16),
+            Functions::randomString(16)
+        );
+    }
+
+}
