90284502044c880d3d7ed6b29cf28d98aecfbc20
Vic Cherubini <vic@leftnode.org>
2015-10-31 23:42:42 -0500

    Removed some unecessary use statements.

--- src/Orderware/AppBundle/Library/Orders/Importer.php
+++ src/Orderware/AppBundle/Library/Orders/Importer.php
@@ -3,8 +3,6 @@
 namespace Orderware\AppBundle\Library\Orders;
 
 use Orderware\AppBundle\Entity\OrdImport;
-#use Orderware\AppBundle\Entity\OrdHeader;
-#use Orderware\AppBundle\Entity\OrdLine;
 use Orderware\AppBundle\Library\Status;
 use Orderware\AppBundle\Library\Utils;
 
