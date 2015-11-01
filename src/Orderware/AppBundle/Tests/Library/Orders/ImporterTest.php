ebed0762f862b55901540d461f5e475dd2423182
Vic Cherubini <vic@leftnode.org>
2015-11-01 00:42:20 -0500

    Added more tests for the order importer.

--- src/Orderware/AppBundle/Tests/Library/Orders/ImporterTest.php
+++ src/Orderware/AppBundle/Tests/Library/Orders/ImporterTest.php
@@ -45,4 +45,61 @@ class ImporterTest extends TestCase
         $this->assertEquals($orderDate, $order['order_date']);
     }
 
+    public function testOrderNumMustBeUnique()
+    {
+        $orderBody = file_get_contents(__DIR__ . '/order.no_lines.json');
+        
+        $import = $this->fixtures['ord_import'];
+        $import->setOrderBody($orderBody);
+
+        $importer = $this->getContainer()
+            ->get('orderware.order_importer');
+
+        $importer->import($import);
+
+        $this->assertFalse($import->hasError());
+        $this->assertNull($import->getErrorMessage());
+
+        $importer->import($import);
+
+        $this->assertTrue($import->hasError());
+        $this->assertEquals("The order number (GROW0000001) already exists.", $import->getErrorMessage());
+    }
+
+    public function testOrderNumMustBeAlphaNumeric()
+    {
+        $orderBody = file_get_contents(__DIR__ . '/order.utf8_order_num.json');
+
+        $import = $this->fixtures['ord_import'];
+        $import->setOrderBody($orderBody);
+
+        $importer = $this->getContainer()
+            ->get('orderware.order_importer')
+            ->import($import);
+
+        $this->assertTrue($import->hasError());
+        $this->assertEquals("The order number (Производители0000001) must be alpha-numeric.", $import->getErrorMessage());
+    }
+
+    public function _testLineNumbersMustBeUnique()
+    {
+        $orderBody = file_get_contents(__DIR__ . '/order.duplicate_line_num.json');
+
+    }
+
+    public function testSkuMustExist()
+    {
+        $orderBody = file_get_contents(__DIR__ . '/order.missing_sku.json');
+
+        $import = $this->fixtures['ord_import'];
+        $import->setOrderBody($orderBody);
+
+        $importer = $this->getContainer()
+            ->get('orderware.order_importer')
+            ->import($import);
+
+        $this->assertTrue($import->hasError());
+        $this->assertEquals("The SKU (~) could not be found.", $import->getErrorMessage());
+    }
+
 }
