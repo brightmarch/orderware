f3a6942bc41b7d9e21d8754da1a95446b75f850d
Vic Cherubini <vic@leftnode.org>
2015-11-01 00:29:18 -0500

    Created a new service to load an entire order into an array.

---
+++ src/Orderware/AppBundle/Library/Orders/Loader.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Orderware\AppBundle\Library\Orders;
+
+use Doctrine\ORM\EntityManager;
+
+use \InvalidArgumentException;
+
+class Loader
+{
+
+    /** @var Doctrine\ORM\EntityManager */
+    private $entityManager;
+
+    /** @var array */
+    private $order = [];
+
+    /** @var integer */
+    private $ordId = 0;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function load($division, $orderNum)
+    {
+        $_conn = $this->entityManager
+            ->getConnection();
+
+        // Ensure the order exists first.
+        $this->ordId = $_conn->fetchColumn("SELECT lookup_order(?, ?)", [
+            $division, $orderNum
+        ]);
+
+        if (0 === $this->ordId) {
+            throw new InvalidArgumentException(sprintf("The order number (%s) does not exist.", strtoupper($orderNum)));
+        }
+
+        $sql = "
+            SELECT oh.* FROM ord_header oh
+            WHERE oh.ord_id = ?
+        ";
+
+        $this->order = $_conn->fetchAssoc($sql, [
+            $this->ordId
+        ]);
+
+        return $this->order;
+    }
+
+}
