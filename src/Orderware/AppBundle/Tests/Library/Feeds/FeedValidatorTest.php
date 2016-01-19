44a15a523394a793330ec4308196275c25bc4ad8
Vic Cherubini <vic@leftnode.org>
2016-01-19 00:35:51 -0600

    Working on getting the basic FeedValidator test to work.

---
+++ src/Orderware/AppBundle/Tests/Library/Feeds/FeedValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Orderware\AppBundle\Tests\Library\Feeds;
+
+use Orderware\AppBundle\Tests\TestCase;
+
+class FeedValidatorTest extends TestCase
+{
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
