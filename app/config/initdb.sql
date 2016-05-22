740b3a54c011f74c3459eb5e56ca455910396ea6
Vic Cherubini <vic@brightmarch.com>
2016-05-22 06:41:23 -0500

    Updated the initdb script to write to the correct table.

--- app/config/initdb.sql
+++ app/config/initdb.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
-INSERT INTO division (
-    division, created_at, updated_at, created_by, updated_by, status_id,
+INSERT INTO account (
+    account, created_at, updated_at, created_by, updated_by, status_id,
     display_name, currency, time_zone, primary_email,
     notification_email, merch_description
-) SELECT 'GROWERS', LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'orderware', 'orderware', 1,
+) SELECT 'GROWERS', LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1,
     'Growers Coffee', 'USD', 'America/Chicago', 'orders@growerscoffee.com',
     'orders@growerscoffee.com', 'organic coffee'
-FROM division HAVING COUNT(division) = 0;
+FROM account HAVING COUNT(account) = 0;
 
 COMMIT;
