5fce56c67b98c4f10e3cf2e44cc9b0d0b33b3a09
Vic Cherubini <vic@brightmarch.com>
2016-05-22 04:56:09 -0500

    Added the main FeedProcessor service to the list of services.

--- src/Orderware/AppBundle/Library/Feeds/FeedProcessor.php
+++ src/Orderware/AppBundle/Library/Feeds/FeedProcessor.php
@@ -2,18 +2,36 @@
 
 namespace Orderware\AppBundle\Library\Feeds;
 
+use Symfony\Component\DependencyInjection\Container;
+
 class FeedProcessor
 {
 
+    /** @var Symfony\Component\DependencyInjection\Container */
     private $container;
 
-    public function __construct($container)
+    /** @var string */
+    private $localFile;
+
+    public function __construct(Container $container)
     {
         $this->container = $container;
     }
 
-    public function process($division, $feedName, $direction)
+    public function process($account, $direction, $feedName)
     {
+        // Get the feed configuration.
+
+        // If inbound, get files.
+
+        // Else if outbound, run feed and generate file.
+    }
+
+    public function setLocalFile($localFile)
+    {
+        $this->localFile = $localFile;
+
+        return $this;
     }
 
 }
