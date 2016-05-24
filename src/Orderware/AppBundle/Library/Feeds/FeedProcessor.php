6f6732f0e78bafa06c9184974a5320e72611233d
Vic Cherubini <vic@brightmarch.com>
2016-05-23 21:11:31 -0500

    Immediately attempts to get the feed configuration when processing a feed.

--- src/Orderware/AppBundle/Library/Feeds/FeedProcessor.php
+++ src/Orderware/AppBundle/Library/Feeds/FeedProcessor.php
@@ -4,11 +4,16 @@ namespace Orderware\AppBundle\Library\Feeds;
 
 use Symfony\Component\DependencyInjection\Container;
 
+use \RuntimeException;
+
 class FeedProcessor
 {
 
     /** @var Symfony\Component\DependencyInjection\Container */
     private $container;
+
+    /** @var Doctrine\ORM\EntityManager */
+    private $entityManager;
 
     /** @var string */
     private $localFile;
@@ -16,10 +21,30 @@ class FeedProcessor
     public function __construct(Container $container)
     {
         $this->container = $container;
+
+        $this->entityManager = $this->container
+            ->get('doctrine')
+            ->getManager('orderware');
     }
 
     public function process($account, $direction, $feedName)
     {
+        // Get the feed configuration.
+        $feed = $this->entityManager
+            ->getRepository('Orderware:Feed')
+            ->findOneBy([
+                'account' => $account,
+                'direction' => $direction,
+                'name' => $feedName
+            ]);
+
+        if (!$feed) {
+            throw new RuntimeException("dick butts");
+        }
+
+
+        // Immediately create a feed log record to ensure this always has a record of running.
+
         // Alias because this is going to be used a lot.
 
         // Get the feed configuration.
