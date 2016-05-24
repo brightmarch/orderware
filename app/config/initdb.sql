58b0877e150eaf1230519d7658b8ddd73dea3d2d
Vic Cherubini <vic@brightmarch.com>
2016-05-23 21:04:48 -0500

    Better initdb.sql script to create a feed as well.

--- app/config/initdb.sql
+++ app/config/initdb.sql
@@ -1,12 +1,9 @@
 BEGIN;
 
-INSERT INTO account (
-    account, created_at, updated_at, created_by, updated_by, status_id,
-    display_name, currency, time_zone, primary_email,
-    notification_email, merch_description
-) SELECT 'GROWERS', LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1,
-    'Growers Coffee', 'USD', 'America/Chicago', 'orders@growerscoffee.com',
-    'orders@growerscoffee.com', 'organic coffee'
-FROM account HAVING COUNT(account) = 0;
+-- Account
+INSERT INTO account (account, created_at, updated_at, created_by, updated_by, status_id, display_name, currency, time_zone, primary_email, notification_email, merch_description) VALUES ('GROWERS', LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1, 'Growers Coffee', 'USD', 'America/Chicago', 'orders@growerscoffee.com', 'orders@growerscoffee.com', 'organic coffee');
+
+-- Feeds
+INSERT INTO feed (feed_id, created_at, updated_at, created_by, updated_by, status_id, direction, name, service, remote_root_dir, local_root_dir, filename) VALUES (default, LOCALTIMESAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1, 'inbound', 'item', 'orderware.feeds.item_feed_processor', '/var/apps/data/remote/catalog', '/var/apps/data/local/catalog', '/growers_item_(.*).xml/');
 
 COMMIT;
