1dbf9478f874c5bca7d15cd155dec34bdf81dc15
Vic Cherubini <vic@brightmarch.com>
2016-05-23 21:07:13 -0500

    Fixed the SQL method for LOCALTIMESTAMP(0) and added the account column so the queries execute.

--- app/config/initdb.sql
+++ app/config/initdb.sql
@@ -4,6 +4,6 @@ BEGIN;
 INSERT INTO account (account, created_at, updated_at, created_by, updated_by, status_id, display_name, currency, time_zone, primary_email, notification_email, merch_description) VALUES ('GROWERS', LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1, 'Growers Coffee', 'USD', 'America/Chicago', 'orders@growerscoffee.com', 'orders@growerscoffee.com', 'organic coffee');
 
 -- Feeds
-INSERT INTO feed (feed_id, created_at, updated_at, created_by, updated_by, status_id, direction, name, service, remote_root_dir, local_root_dir, filename) VALUES (default, LOCALTIMESAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 1, 'inbound', 'item', 'orderware.feeds.item_feed_processor', '/var/apps/data/remote/catalog', '/var/apps/data/local/catalog', '/growers_item_(.*).xml/');
+INSERT INTO feed (feed_id, created_at, updated_at, created_by, updated_by, account, status_id, direction, name, service, remote_root_dir, local_root_dir, filename) VALUES (default, LOCALTIMESTAMP(0), LOCALTIMESTAMP(0), 'initdb', 'initdb', 'GROWERS', 1, 'inbound', 'item', 'orderware.feeds.item_feed_processor', '/var/apps/data/remote/catalog', '/var/apps/data/local/catalog', '/growers_item_(.*).xml/');
 
 COMMIT;
